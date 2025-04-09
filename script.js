800f49c163d04416d97b98c72fb54167cfd26ff6
Ciolac Sandu <ciolacsandu64@gmail.com>
2025-04-09 10:35:07 +0300

    Am inceput sa scriu intrebarile

---
+++ script.js
@@ -0,0 +1,39 @@
+const question = [
+    {
+        question: "Care este cel mai mare animal din lume?",
+        answer: [
+          {text: "Rechinul",correct: false},
+          {text: "Elefant",correct: false},
+          {text: "Balena albastra",correct: true},
+          {text: "Girafa",correct: false},
+        ]
+    },
+    {
+         question: "Care planetă este cunoscută ca,,planeta roșie''?",
+        answer: [
+          {text: "Venus",correct: false},
+          {text: "Marte",correct: true},
+          {text: "Jupiter",correct: true},
+          {text: "Mercur",correct: false},
+        ]
+    },
+    {
+         question: "Ce element chimic are simbolul,,O''?",
+        answer: [
+          {text: "Osmium",correct: false},
+          {text: "Aur",correct: false},
+          {text: "Oxigen",correct: true},
+          {text: "Hidrogen",correct: false},
+        ]
+    },
+    {
+         question: "Ce fenomen natural poate cauza o „lumina albastră” pe cer în timpul nopții?",
+        answer: [
+          {text: "Aurora boreala",correct: true},
+          {text: "Fulgerele",correct: false},
+          {text: "Norii selenari",correct: true},
+          {text: "Eclipsele lunare",correct: false},
+        ]
+    }
+];
+
