3882959288bb16853ba1841b38c453dbeeb005c7
Ciolac Sandu <ciolacsandu64@gmail.com>
2025-04-17 10:24:31 +0300

    Am actualizat Dashboard

--- Dashboard/dash.js
+++ Dashboard/dash.js
@@ -1,20 +1,16 @@
-
-document.getElementById("easy").addEventListener("click", function() {
+document.getElementById("easy").addEventListener("click", function () {
     window.location.href = "../quiz/easy.html";
-});
-
-document.getElementById("normal").addEventListener("click", function() {
+  });
+  
+  document.getElementById("normal").addEventListener("click", function () {
     window.location.href = "../quiz/normal.html";
-});
-
-document.getElementById("hard").addEventListener("click", function() {
+  });
+  
+  document.getElementById("hard").addEventListener("click", function () {
     window.location.href = "../quiz/hard.html";
-});
-
-document.getElementById("very-hard").addEventListener("click", function() {
-    window.location.href = "../quiz/veryhard.html";
-});
-document.querySelector(".logout-btn").addEventListener("click", function() {
-    sessionStorage.removeItem("loggedIn");
-    window.location.href = "../login.html";
-});
+  });
+  
+  document.getElementById("very-hard").addEventListener("click", function () {
+    window.location.href = "../quiz/very hard.html";
+  });
+  
