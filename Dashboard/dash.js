ebb8c1752b8fa5186051b424700e92c32c474a9a
Ciolac Sandu <ciolacsandu64@gmail.com>
2025-04-26 13:32:04 +0300

    Am actualizat dashboard

--- Dashboard/dash.js
+++ Dashboard/dash.js
@@ -1,16 +1,23 @@
 document.getElementById("easy").addEventListener("click", function () {
-    window.location.href = "../quiz/easy.html";
-  });
-  
-  document.getElementById("normal").addEventListener("click", function () {
-    window.location.href = "../quiz/normal.html";
-  });
-  
-  document.getElementById("hard").addEventListener("click", function () {
-    window.location.href = "../quiz/hard.html";
-  });
-  
-  document.getElementById("very-hard").addEventListener("click", function () {
-    window.location.href = "../quiz/very hard.html";
-  });
-  
+  window.location.href = "../quiz/easy.html";
+});
+
+document.getElementById("normal").addEventListener("click", function () {
+  window.location.href = "../quiz/normal.html";
+});
+
+document.getElementById("hard").addEventListener("click", function () {
+  window.location.href = "../quiz/hard.html";
+});
+
+document.getElementById("very-hard").addEventListener("click", function () {
+  window.location.href = "../quiz/very hard.html";
+});
+
+document.getElementById("math").addEventListener("click", function () {
+  window.location.href = "../quiz/math.html";
+});
+
+document.getElementById("geography").addEventListener("click", function () {
+  window.location.href = "../quiz/geography.html";
+});
